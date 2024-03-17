9983abf29195d1af4b6e7b5b38ae562192d9497c
Wesley Lima <wesley.lima@invitatech.com.br>
2024-03-17 19:10:57 -0300

    Implementação do styled-components e desenvolvimento do arquivo de tema para cores e fontes.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+
+import THEME from "./src/global/styles/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={THEME}>
+      <View style={styles.container}>
+        <StatusBar backgroundColor={THEME.colors.primary} />
+        <Text style={styles.text}>Hinário Virtual</Text>
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: THEME.colors.primary,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: THEME.colors.light,
+    fontSize: THEME.fontSize.normal,
+    fontWeight: "bold",
   },
 });
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,30 @@
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default {
+  colors: {
+    primary: "#4D0302",
+    dark: "#373737",
+    dark_dark: "#282828",
+    dark_light: "#363636",
+    light: "#FFFFFF",
+    light_dark: "#EDE8E8",
+    light_light: "#F8FAFF",
+    gray: "#ABABAB",
+    gray_dark: "#777777",
+    gray_light: "#EEEEEE",
+    danger: "#D04B38",
+    warning: "#FAB032",
+    success: "#0FB746",
+  },
+  fonts: {
+    title: "Roboto_700Bold",
+    text: "Roboto_400Regular",
+  },
+  fontSize: {
+    smallest: RFValue(11),
+    smaller: RFValue(12),
+    normal: RFValue(14),
+    larger: RFValue(18),
+    largest: RFValue(22),
+  },
+};
